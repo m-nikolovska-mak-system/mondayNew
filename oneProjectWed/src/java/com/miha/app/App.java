16a2fdb4a949d79e0858362f4d0169c94c69d029
m-nikolovska-mak-system <m.nikolovska@mak-system.net>
2025-10-13 11:08:14 +0200

    Initial commit with fixed Gradle structure

---
+++ oneProjectWed/src/java/com/miha/app/App.java
@@ -0,0 +1,22 @@
+package com.miha.app;
+//import  com.app.java.com.miha.app;
+
+
+
+public class App {
+	
+	/*
+    public String getGreeting() {
+        return "CustomLib identifier is: " + SecondProject.identifier;
+    }
+*/
+
+	public static void main(String[] args) {
+		System.out.println("Hello!!!");
+		System.out.println("Hello too!!");
+
+
+       // System.out.println(new App().getGreeting());
+
+	}
+}
