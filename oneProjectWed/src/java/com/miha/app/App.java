a255ab2f3a1a5f4b30870bb2684d3cea5659c184
m-nikolovska-mak-system <m.nikolovska@mak-system.net>
2025-10-16 17:12:45 +0200

    Fix typo in greeting message

--- oneProjectWed/src/java/com/miha/app/App.java
+++ oneProjectWed/src/java/com/miha/app/App.java
@@ -12,7 +12,7 @@ public class App {
 */
 
 	public static void main(String[] args) {
-		System.out.println("Hellooooo!!!");
+		System.out.println("Heellooooo!!!");
 		System.out.println("Hello this is a test for jira automation on codechecker!!");
 		System.out.println("this is NOT supposed to fail and NOT notify teams");
 
