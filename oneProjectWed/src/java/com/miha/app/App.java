8077cf5c4cb67cc58606a21ecab20425288196ab
Mihaela Nikolovska <nikolovskabr@gmail.com>
2025-10-20 11:30:36 +0200

    testing to see if repo is connected

--- oneProjectWed/src/java/com/miha/app/App.java
+++ oneProjectWed/src/java/com/miha/app/App.java
@@ -14,7 +14,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Heellooo0oo!!!");
 		System.out.println("Hello this is a test for jira automation on codechecker!!");
-		System.out.println("this is CHANGE HI supposed to fail and NOT notify teams");
+		System.out.println("this is another test ppush hello from monday");
 
 
 
