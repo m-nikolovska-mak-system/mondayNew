4cd545f0d8419878e73d1f3e5d734f051fe7e6c4
m-nikolovska-mak-system <m.nikolovska@mak-system.net>
2025-10-20 16:59:39 +0200

    deleted subfolder

--- oneProjectWed/src/java/com/miha/app/App.java
+++ oneProjectWed/src/java/com/miha/app/App.java
@@ -14,7 +14,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Heellooo0oo!!!");
 		System.out.println("Hello this is a test for jira automation on codechecker!!");
-		System.out.println("this is test hi  test hi hello push hello from tuesdat hiii!!");
+		System.out.println("this is test hi lalal  test hi hello push hello from tuesdat hiii!!");
 
 
 
