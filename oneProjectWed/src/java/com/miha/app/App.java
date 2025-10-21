ae8ea37cc479aee5b7ae29be739b0b86095e03a2
m-nikolovska-mak-system <m.nikolovska@mak-system.net>
2025-10-21 09:59:16 +0200

    Update App.java

--- oneProjectWed/src/java/com/miha/app/App.java
+++ oneProjectWed/src/java/com/miha/app/App.java
@@ -14,7 +14,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Heellooo0oo!!!");
 		System.out.println("Hello this is a test for jira automation on codechecker!!");
-		System.out.println("this is test hi  hhi hello push hello from tuesdat hiii!!");
+		System.out.println("this is test hi test hello push hello from tuesdat hiii!!");
 
 
 
