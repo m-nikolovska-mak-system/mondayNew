9f95afc058fc42f395b5d4c32111e5b5f9c8e5e0
m-nikolovska-mak-system <m.nikolovska@mak-system.net>
2025-10-27 11:23:00 +0100

    Update App.java

--- oneProjectWed/src/java/com/miha/app/App.java
+++ oneProjectWed/src/java/com/miha/app/App.java
@@ -14,7 +14,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Heellooo0oo!!!");
 		System.out.println("Hello this is a test for jira automation hello this is me changing the file on codechecker!!");
-		System.out.println("this iaaaaaasssshiijjjjjjiaaaa hello test hi hello new hiaaaaaaaaaaii test fridya! hiii hiii for functions! hii branching");
+		System.out.println("this hello test hi hello new hiaaaaaaaaaaii test fridya! hiii hiii for functions! hii branching");
 
 
 
