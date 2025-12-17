1219b1223a7f003d15268808f1ecfaec1f92e272
m-nikolovska-mak-system <m.nikolovska@mak-system.net>
2025-12-17 10:30:05 +0100

    Update generate_readme.py

--- scripts/generate_readme.py
+++ scripts/generate_readme.py
@@ -3,7 +3,7 @@ import yaml
 from datetime import datetime
 from pathlib import Path
 
-# ConfigurationNSS yhis isaaaaa
+# ConfigurationNSS yhis isaaaaaaaa
 CONFIG_PATH = "action.yml"  # or .yaml
 README_PATH = "README.md"
 
